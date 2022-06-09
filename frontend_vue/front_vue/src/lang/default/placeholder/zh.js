742c01c6429d3e12b5de76feb4fa07ae2c5d27af
WatsonJay <446725026@qq.com>
2022-06-09 19:48:08 +0800

    rebuild

--- frontend_vue/front_vue/src/lang/default/placeholder/zh.js
+++
@@ -1,9 +0,0 @@
-export default {
-  username: "用户名",
-  password: "密码",
-  nickname: "昵称",
-  checkpassword: "确认密码",
-  Email: "邮箱",
-  phone: "电话",
-  searchRouter: "查询菜单"
-}
--- frontend_vue/front_vue/src/store/getters.js
+++
@@ -1,7 +0,0 @@
-const getters = {
-  language: state => state.app.language,
-  sidebar: state => state.app.sidebar,
-  device: state => state.app.device,
-  user: state => state.user.user
-};
-export default getters;
