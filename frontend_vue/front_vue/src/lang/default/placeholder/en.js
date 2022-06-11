f5ad7d50021ade1e97c4db71dbc32b0cc3dc43f3
WatsonJay <446725026@qq.com>
2022-06-11 23:29:57 +0800

    rebuild

--- frontend_vue/front_vue/src/lang/default/placeholder/en.js
+++
@@ -1,9 +0,0 @@
-export default {
-  username: "Username",
-  password: "Password",
-  nickname: "nickname",
-  checkpassword: "checkpassword",
-  Email: "Email",
-  phone: "phone",
-  searchRouter: "Search Router"
-}
