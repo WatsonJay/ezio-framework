2366584f4d29adec14b837c482e69a22f3f238e1
WatsonJay <446725026@qq.com>
2022-06-26 16:57:41 +0800

    feat: 基础框架更新

---
+++ ezio-antd-vue/src/core/use.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+
+// base library
+import Antd, { version } from 'ant-design-vue';
+console.log('ant-design-vue version:', version)
+import 'ant-design-vue/dist/antd.css';  // or 'ant-design-vue/dist/antd.less'
+
+Vue.use(Antd)
+
+process.env.NODE_ENV !== 'production' && console.warn('[antd-pro] WARNING: Antd now use fulled imported.')
