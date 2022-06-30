edfb8fe37696676bf28e0e4c6874a695f767efd9
WatsonJay <446725026@qq.com>
2022-06-30 10:37:37 +0800

    feat: 引入filter和rules

--- ezio-antd-vue/src/main.js
+++ ezio-antd-vue/src/main.js
@@ -4,6 +4,13 @@ import router from "./router";
 import store from "./store";
 
 import "./core/use"; // use lazy load components
+import "./permission" // permission control
+import "./utils/filter" // base filter
+// import Print from 'vue-print-nb-jeecg'
+import './global.sass' // global style
+//表单验证
+import { rules } from '@/utils/rules'
+Vue.prototype.rules = rules
 
 Vue.config.productionTip = false;
 
