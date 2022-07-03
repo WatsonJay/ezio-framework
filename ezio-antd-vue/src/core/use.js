2fc1ebf9a275b7cec64ac02e081bcca98b4f4d7e
WatsonJay <446725026@qq.com>
2022-07-03 10:27:39 +0800

    feat: eslint规则更新，不合规修改

--- ezio-antd-vue/src/core/use.js
+++ ezio-antd-vue/src/core/use.js
@@ -1,11 +1,11 @@
-import Vue from "vue";
+import Vue from 'vue'
 
 // base library
-import Antd, { version } from "ant-design-vue";
-console.log("ant-design-vue version:", version);
-import "ant-design-vue/dist/antd.css"; // or 'ant-design-vue/dist/antd.less'
+import Antd, { version } from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+console.log('ant-design-vue version:', version) // or 'ant-design-vue/dist/antd.less'
 
-Vue.use(Antd);
+Vue.use(Antd)
 
-process.env.NODE_ENV !== "production" &&
-  console.warn("[antd-pro] WARNING: Antd now use fulled imported.");
+process.env.NODE_ENV !== 'production' &&
+  console.warn('[antd-pro] WARNING: Antd now use fulled imported.')
--- ezio-antd-vue/src/main.js
+++ ezio-antd-vue/src/main.js
@@ -1,21 +1,25 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-import "./core/use"; // use lazy load components
-import "./permission" // permission control
-import "./utils/filter" // base filter
+// mock
+// WARNING: `mockjs` NOT SUPPORT `IE` PLEASE DO NOT USE IN `production` ENV.
+import './mock'
+
+import './core/use' // use lazy load components
+import './permission' // permission control
+import './utils/filter' // base filter
 // import Print from 'vue-print-nb-jeecg'
 import './global.sass' // global style
-//表单验证
+
+// 表单验证
 import { rules } from '@/utils/rules'
 Vue.prototype.rules = rules
-
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
-}).$mount("#app");
+  render: (h) => h(App)
+}).$mount('#app')
